6b3ae81549a490513568407ec599333e84fbcf66
Md.Fahim <mortuza.aziz.47@gmail.com>
2025-08-06 12:28:32 +0600

    Update Title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 function Header() {
-  return <div className="text-3xl text-slate-600">Task Manager</div>;
+  return <div className="text-3xl text-slate-600">Todo-List</div>;
 }
 
 export default Header;
